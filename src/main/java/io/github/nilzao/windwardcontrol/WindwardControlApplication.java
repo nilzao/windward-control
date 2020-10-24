dfe3cf4651a36b207a682cc8635e474e2890e048
Nilzao <288571+nilzao@users.noreply.github.com>
2020-10-24 07:29:10 -0400

    spring initializr

---
+++ src/main/java/io/github/nilzao/windwardcontrol/WindwardControlApplication.java
@@ -0,0 +1,13 @@
+package io.github.nilzao.windwardcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindwardControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindwardControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/nilzao/windwardcontrol/WindwardControlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nilzao.windwardcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindwardControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
